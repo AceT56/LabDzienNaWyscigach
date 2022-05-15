edb8cd0ec53c2db66b8c626d6da87716b8770c03
AceT56 <31236737+AceT56@users.noreply.github.com>
2022-05-15 14:44:22 +0200

    chars change

--- LabDzienNaWyscigach/Properties/AssemblyInfo.cs
+++ LabDzienNaWyscigach/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LabDzieńNaWyścigach")]
+[assembly: AssemblyTitle("LabDzienNaWyscigach")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LabDzieńNaWyścigach")]
+[assembly: AssemblyProduct("LabDzienNaWyscigach")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
